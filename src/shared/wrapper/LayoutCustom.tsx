39e911a07c609ae3bc0cfe8404ba9be349637ece
parag <pkakabari22@gmail.com>
2024-06-15 12:28:44 +0530

    route set

--- src/shared/wrapper/LayoutCustom.tsx
+++ src/shared/wrapper/LayoutCustom.tsx
@@ -8,30 +8,40 @@ import styles from "./LayoutCustom.module.scss";
 
 export const LayoutCustom = ({ children }) => {
   const path = usePathname();
-  console.log("path", path);
 
   const sidebarRoutes = ["/dashboard"];
   const headerRoutes = ["/dashboard"];
   const headerHomeRoutes = ["/"];
+  const loginRoutes = ["/login", "/signup"];
 
   const isSidebarRoute = sidebarRoutes.includes(path);
   const isHeaderOnlyRoute = headerRoutes.includes(path);
   const isHeaderHomeRoute = headerHomeRoutes.includes(path);
+  const login = loginRoutes.includes(path);
+
 
   return (
     <>
-      <div className={styles.layoutCustomSection}>
-        <div className={styles.layoutCustomLeft}>
-          {isSidebarRoute && <Sidebar />}
-        </div>
-        <div className={styles.layoutCustomRight}>
+      {isSidebarRoute && (
+        <div className={styles.layoutCustomSection}>
+          <div className={styles.layoutCustomLeft}>
+            {isSidebarRoute && <Sidebar />}
+          </div>
+          <div className={styles.layoutCustomRight}>
+            {isHeaderOnlyRoute && <Header />}
 
-          {isHeaderOnlyRoute && <Header />}
+            {children}
+          </div>
+        </div>
+      )}
+      {isHeaderHomeRoute && (
+        <>
           {isHeaderHomeRoute && <HomeHeader />}
 
           {children}
-        </div>
-      </div>
+        </>
+      )}
+      {login && <>{children}</>}
     </>
   );
 };
