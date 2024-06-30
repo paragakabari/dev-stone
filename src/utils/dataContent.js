e40fb3958f7b158e94d0918ce59d034e4cae225f
abhishekglidetechsolutions <abhishek@glidetechsolutions.com>
2024-06-29 22:46:11 -0700

    add: google login

---
+++ src/utils/dataContent.js
@@ -0,0 +1,355 @@
+export const IndustryOption = [
+    {
+        "label": "Aerospace",
+        "value": "aerospace"
+    },
+    {
+        "label": "Agri-Food",
+        "value": "agri-food"
+    },
+    {
+        "label": "Agri-Food/Food Processing",
+        "value": "agri-food-food-processing"
+    },
+    {
+        "label": "Agriculture",
+        "value": "agriculture"
+    },
+    {
+        "label": "Architecture/Design",
+        "value": "architecture-design"
+    },
+    {
+        "label": "Artificial Intelligence",
+        "value": "artificial-intelligence"
+    },
+    {
+        "label": "Arts & Culture",
+        "value": "arts-culture"
+    },
+    {
+        "label": "Automotive",
+        "value": "automotive"
+    },
+    {
+        "label": "Aviation",
+        "value": "aviation"
+    },
+    {
+        "label": "Biomedical",
+        "value": "biomedical"
+    },
+    {
+        "label": "Biotechnology",
+        "value": "biotechnology"
+    },
+    {
+        "label": "Breweries/Wineries",
+        "value": "breweries-wineries"
+    },
+    {
+        "label": "Childcare",
+        "value": "childcare"
+    },
+    {
+        "label": "Clean Energy",
+        "value": "clean-energy"
+    },
+    {
+        "label": "Clean Tech",
+        "value": "clean-tech"
+    },
+    {
+        "label": "Community/Social Change",
+        "value": "community-social-change"
+    },
+    {
+        "label": "Construction",
+        "value": "construction"
+    },
+    {
+        "label": "E-Commerce",
+        "value": "e-commerce"
+    },
+    {
+        "label": "Educational Services",
+        "value": "educational-services"
+    },
+    {
+        "label": "Electrical",
+        "value": "electrical"
+    },
+    {
+        "label": "Energy",
+        "value": "energy"
+    },
+    {
+        "label": "Engineering",
+        "value": "engineering"
+    },
+    {
+        "label": "Environmental Sustainability",
+        "value": "environmental-sustainability"
+    },
+    {
+        "label": "Film/Music/Entertainment",
+        "value": "film-music-entertainment"
+    },
+    {
+        "label": "Financial",
+        "value": "financial"
+    },
+    {
+        "label": "Firearms",
+        "value": "firearms"
+    },
+    {
+        "label": "Forestry",
+        "value": "forestry"
+    },
+    {
+        "label": "Healthcare",
+        "value": "healthcare"
+    },
+    {
+        "label": "Hospitality",
+        "value": "hospitality"
+    },
+    {
+        "label": "Housing",
+        "value": "housing"
+    },
+    {
+        "label": "Human Resources",
+        "value": "human-resources"
+    },
+    {
+        "label": "Hunting/Fishing",
+        "value": "hunting-fishing"
+    },
+    {
+        "label": "IP",
+        "value": "ip"
+    },
+    {
+        "label": "IT",
+        "value": "it"
+    },
+    {
+        "label": "Industrial",
+        "value": "industrial"
+    },
+    {
+        "label": "Infrastructure",
+        "value": "infrastructure"
+    },
+    {
+        "label": "Life Sciences",
+        "value": "life-sciences"
+    },
+    {
+        "label": "Manufacturing",
+        "value": "manufacturing"
+    },
+    {
+        "label": "Marine",
+        "value": "marine"
+    },
+    {
+        "label": "Marketing",
+        "value": "marketing"
+    },
+    {
+        "label": "Materials",
+        "value": "materials"
+    },
+    {
+        "label": "Math",
+        "value": "math"
+    },
+    {
+        "label": "Mining",
+        "value": "mining"
+    },
+    {
+        "label": "Nature/Environment",
+        "value": "nature-environment"
+    },
+    {
+        "label": "Oil & Gas Extraction",
+        "value": "oil-gas-extraction"
+    },
+    {
+        "label": "Operations",
+        "value": "operations"
+    },
+    {
+        "label": "Physical Sciences",
+        "value": "physical-sciences"
+    },
+    {
+        "label": "Professional Services and Consulting",
+        "value": "professional-services-and-consulting"
+    },
+    {
+        "label": "Real Estate",
+        "value": "real-estate"
+    },
+    {
+        "label": "Research Disciplines",
+        "value": "research-disciplines"
+    },
+    {
+        "label": "Safety",
+        "value": "safety"
+    },
+    {
+        "label": "Sales",
+        "value": "sales"
+    },
+    {
+        "label": "Security",
+        "value": "security"
+    },
+    {
+        "label": "Software",
+        "value": "software"
+    },
+    {
+        "label": "Sports",
+        "value": "sports"
+    },
+    {
+        "label": "Supply Chain/Logistics",
+        "value": "supply-chain-logistics"
+    },
+    {
+        "label": "Technology",
+        "value": "technology"
+    },
+    {
+        "label": "Telecommunications",
+        "value": "telecommunications"
+    },
+    {
+        "label": "Tobacco/Vaping/Cannabis",
+        "value": "tobacco-vaping-cannabis"
+    },
+    {
+        "label": "Tourism",
+        "value": "tourism"
+    },
+    {
+        "label": "Trades",
+        "value": "trades"
+    },
+    {
+        "label": "Transportation",
+        "value": "transportation"
+    },
+    {
+        "label": "Utilities",
+        "value": "utilities"
+    },
+    {
+        "label": "Warehousing",
+        "value": "warehousing"
+    }
+]
+
+export const FounderDemographic=[
+    {
+        "label": "Black",
+        "value": "Black"
+    },
+    {
+        "label": "Female",
+        "value": "Female"
+    },
+     {
+        "label": "Indigenous",
+        "value": "Indigenous"
+    }, 
+    {
+        "label": "LGBTQ2+",
+        "value": "LGBTQ2+"
+    }, 
+    {
+        "label": "Located in a Rural Area",
+        "value": "Located in a Rural Area"
+    },
+    {
+        "label": "Newcomer",
+        "value": "Newcomer"
+    },
+    {
+        "label": "Persons with disabilities",
+        "value": "Persons with disabilities"
+    },
+    {
+        "label": "Postdoc",
+        "value": "Postdoc"
+    },
+    {
+        "label": "Student",
+        "value": "Student"
+    },
+    {
+        "label": "Veteran",
+        "value": "Veteran"
+    },
+    {
+        "label": "Visible Minority",
+        "value": "Visible Minority"
+    },
+    {
+        "label": "Youth",
+        "value": "Youth"
+    }
+]
+export const RevenueRange = [
+    {
+        "label": "$0 - $29k",
+        "value": "$0 - $29k"
+    },
+    {
+        "label": "$30k - $49k",
+        "value": "$30k - $49k"
+    },
+    {
+        "label": "$50k - $99k",
+        "value": "$50k - $99k"
+    },
+    {
+        "label": "$100K - $249K",
+        "value": "$100K - $249K",
+    },
+    {
+        "label": "$250k - $499k",
+        "value": "$250k - $499k"
+    },
+    {
+        "label": "$500k - $999k",
+        "value": "$500k - $999k"
+    },
+    {
+        "label": "$1M - $1.9M",
+        "value": "$1M - $1.9M",
+    },
+    {
+        "label": "$3M - $99M",
+        "value": "$3M - $99M",
+    },
+    {
+        "label": "$100M or more",
+        "value": "$100M or more",
+    },
+    {
+        "label": "Pre-revenue",
+        "value": "pre-revenue"
+    },
+    {
+        "label": "I d\on't want to say",
+        "value": "I don't want to say",
+    },
+]
