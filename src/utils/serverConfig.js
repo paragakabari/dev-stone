4802bfaeaa3bede6a1ee4295c5ac65f170f0b811
abhishekglidetechsolutions <abhishek@glidetechsolutions.com>
2024-06-29 22:47:23 -0700

    add: auth flow

---
+++ src/utils/serverConfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  localhost: {
+    BASE_URL: "http://127.0.0.1:5001/",
+  },
+  sandbox: {
+    BASE_URL: "https://foundly-services-production.up.railway.app/",
+  },
+  beta: {
+    BASE_URL: "https://foundly-services-production.up.railway.app/",
+  },
+  production: {
+    // BASE_URL: 'http://localhost:4000/',
+    BASE_URL: "https://foundly-services-production.up.railway.app/",
+  },
+};
