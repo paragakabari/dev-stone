78b7f792a2b51cff9463ea36b59a70bfa2fdbb01
denish-gts <denish@glidetechsolutions.com>
2024-07-03 01:31:28 -0400

    updated

--- src/utils/serverConfig.js
+++ src/utils/serverConfig.js
@@ -3,13 +3,13 @@ module.exports = {
     BASE_URL: "http://127.0.0.1:5001/",
   },
   sandbox: {
-    BASE_URL: "https://foundly-services-production.up.railway.app/",
+    BASE_URL: "https://sg-aggregator-backend-production.up.railway.app/",
   },
   beta: {
-    BASE_URL: "https://foundly-services-production.up.railway.app/",
+    BASE_URL: "https://sg-aggregator-backend-production.up.railway.app/",
   },
   production: {
     // BASE_URL: 'http://localhost:4000/',
-    BASE_URL: "https://foundly-services-production.up.railway.app/",
+    BASE_URL: "https://sg-aggregator-backend-production.up.railway.app/",
   },
 };
